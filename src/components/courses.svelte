70138bb0be49a3b09eb7ba47480c0dae27e55f99
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2021-01-14 23:44:02 +0530

    V1 Test

--- src/components/courses.svelte
+++ src/components/courses.svelte
@@ -1,8 +1,49 @@
 <script>
-    import courses from "../data/courses.json";
+    import crs from "./courses.json";
 </script>
 
+<style type="text/scss">
+    section {
+        padding: 10px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+    .course {
+        display: flex;
+        margin: 10px;
+        width: 550px;
+        height: 200px;
+        background: #fff;
+        border: 1px solid #aaa;
+        div,
+        p {
+            padding: 5px;
+        }
+        img {
+            width: 200px;
+            height: 200px;
+            object-fit: cover;
+        }
+        cursor: pointer;
+        transition: transform 0.2s ease;
+        &:hover {
+            transform: scale(1.03);
+        }
+    }
+</style>
+
 <section>
-    <!-- {#each courses as crs}{crs.short}{/each} -->
-    Hi
+    {#each crs as cr}
+        <a href="/courses/{cr.page}">
+            <div class="course">
+                <img src={cr.image} alt="" />
+                <div>
+                    <div style="font-weight:600">{cr.long}</div>
+                    <div>{cr.short} ({cr.time})</div>
+                    <p>{cr.about}</p>
+                </div>
+            </div>
+        </a>
+    {/each}
 </section>
--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -3,26 +3,32 @@
 
 <style type="text/scss">
     nav {
-        width: 100%;
+        background: #fff;
+        width: calc(100% - 20px);
         padding: 10px;
         display: flex;
         justify-content: space-between;
         .part {
             display: flex;
         }
-    }
-    a,
-    a:visited,
-    a:hover {
-        text-decoration: none;
-        color: unset;
+        a {
+            width: 75px;
+            text-align: center;
+            margin: 10px;
+            background: #fff;
+            padding: 10px;
+            outline: 2px solid #fff;
+            text-decoration: underline;
+            border: 2px solid #aaa;
+            transition: all 0.3s ease;
+            &:hover {
+                background: #eee;
+                text-decoration: none;
+            }
+        }
     }
 </style>
 
 <nav>
-    <div class="part">
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/qiskit">QISKit</a>
-    </div>
+    <div class="part"><a href="/">Home</a> <a href="/qiskit">QISKit</a></div>
 </nav>
