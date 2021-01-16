eee5070760ffbfe8814887fb14a9be8216351e9b
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2021-01-16 12:51:54 +0530

    Try 2

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { exec } = require( 'child_process' );
 let port = process.env.PORT || 80;
 
-exec( 'npm run build --port ' + port, ( e, o, s ) => {
+console.log( port );
+exec( 'npm run start --port ' + port, ( e, o, s ) => {
     console.log( e );
     console.log( o );
     console.log( s );
