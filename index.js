c27fdb6772612a54c8c7f28641cea958d30cd095
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2021-01-16 13:09:38 +0530

    Add express to text

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const express = require( 'express' );
+const app = express();
 const { exec } = require( 'child_process' );
 let port = process.env.PORT || 80;
+
+
+app.get( '/', ( req, res ) => {
+    res.send( 'Express working on' + port );
+} )
+
+app.listen( port, () => {
+    console.log( 'Express running' );
+} )
 
 console.log( port );
 exec( 'npm run start --port ' + port, ( e, o, s ) => {
