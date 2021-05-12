7fcffeea4ad402d4ca79dafd3ae5807e9bcaf68d
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2021-05-12 20:07:31 +0530

    Version Bump

---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+// const static = require( '@sveltejs/adapter-static' );
+import statix from '@sveltejs/adapter-static';
+// import node from '@sveltejs/adapter-node';
+import sveltePreprocess from 'svelte-preprocess';
+
+const config = {
+    preprocess: sveltePreprocess(),
+    kit: {
+        adapter: statix( {
+            pages: 'docs',
+            assets: 'docs',
+            fallback: null
+        } ),
+        target: 'body',
+        // paths: {
+        //     base: '/',
+        //     assets: '/assets/'
+        // },
+    }
+};
+
+export default config;
